122243a8f5011c1ae46f10632fbcc2f063074e7c
Marcus Birkin <marcus.birkin@gmail.com>
2021-01-28 19:12:29 +0000

    Ratified document updates

--- src/otp.cpp
+++ src/otp.cpp
@@ -28,15 +28,15 @@ namespace OTPLib {
     QString getAbout()
     {
         return QString(
-                   "<style> h1,h2,h3,h4 { text-align: center; }</style>"
-                   "<p><h1>%1</h1>"
-                   "<h2>Version: %2</h2>"
-                   "<h3>A QT interface for E1.59 (Entertainment Technology Object Transform Protocol (OTP))</h3>"
-                   "<h4>Marcus Birkin 2019-2020</h4>"
-                   "<h4><a href=\"https://github.com/marcusbirkin/OTPLib\">https://github.com/marcusbirkin/OTPLib</a></h4></p>")
-               .arg(QString("OTPLib"))
-               .arg(getVersion())
-               .append(
+                    "<style> h1,h2,h3,h4,h5 { text-align: center; }</style>"
+                    "<p><h1>%1</h1>"
+                    "<h2>Version: %2</h2>"
+                    "<h3>A QT library for ANSI E1.59:2021</h3>"
+                    "<h4>Entertainment Technology Object Transform Protocol (OTP)</h4>"
+                    "<h5>Marcus Birkin 2019 - 2021<br>"
+                    "<a href=\"https://github.com/marcusbirkin/OTPView\">https://github.com/marcusbirkin/OTPView</a></h5></p>")
+                .arg(QString("OTPLib"), getVersion())
+                .append(
                    "<p>This program is free software: you can redistribute it and/or modify "
                    "it under the terms of the GNU Lesser General Public License as published by "
                    "the Free Software Foundation, either version 3 of the License, or "
